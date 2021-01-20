d676fd8be1f2abcfdfc780f4a16f72199e7f6f2a
Eduardo Negrão Silva <62072605+EduardoNegrao@users.noreply.github.com>
2021-01-20 17:16:04 -0300

    Etapa1

---
+++ js/main.js
@@ -0,0 +1,69 @@
+//CONDICIONAIS, LAÇOS DE REPETIÇÃO E DATE
+/*
+var idade = prompt("Qual sua idade?");
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+/*
+var count = 0;
+while (count < 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+/*
+var count;
+for(count = 0; count <=5;count++){
+    alert(count);
+}
+*/
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getMonth()+ 1);
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+//LISTA DE DICIONÁRIOS
+/*var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"} ]
+console.log(frutas);
+console.log(frutas[1]);
+console.log(frutas[1].nome);
+*/
+
+//DICIONÁRIO
+/*var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta);
+console.log(fruta.nome);
+console.log(fruta.cor);
+*/
+
+//ARRAY
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//console.log(lista);
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+//console.log(lista[1]);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//alert(lista[1]);
+
+//ALERT CONSOLE.LOG VARIAVEIS
+//var nome = "Eduardo Negrao";
+//var n1 = 21;
+//var n2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 + n2);// */+-
+//console.log(frase.replace("Japão", "Brasil"));
+//console.log(frase.toUpperCase());//toLowerCase()
+//alert(frase.replace("Japão", "Brasil"));
